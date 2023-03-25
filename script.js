79cbbfc8c9653b590b5846397afca4e4a4786c53
SelfandyFajar <90126755+SelfandyFajar@users.noreply.github.com>
2023-03-25 11:47:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,24 @@ function clickButton(){
     const upin = document.querySelectorAll('.upin');
     const ipin = document.querySelectorAll('.ipin');
     const loveAnimation = document.querySelectorAll('.love-animation');
+    const DescriptionAnimation = document.querySelectorAll('.description-animation');
 
     const button = document.querySelector('.btn');
     const borderDiv = document.querySelector('.border');
-
+    var audio = document.querySelector(".audio");
+    
     upin.forEach((nodelist) => {
         nodelist.classList.toggle('transition-out');
     });
-
+    
     ipin.forEach((nodelist) => {
         nodelist.classList.toggle('transition-out');
     });
-
+    
     button.classList.toggle('transition-out');
-
+    
+    audio.play();
+    audio.volume = 0.2;
     setTimeout(() => {
         upin.forEach((nodelist) => {
             nodelist.classList.toggle('hide');
@@ -38,6 +42,12 @@ function clickButton(){
             nodelist.classList.toggle('hide');
             nodelist.classList.toggle('love');
         });
+
+        DescriptionAnimation.forEach((nodelist) => {
+            nodelist.classList.toggle('hide');
+            nodelist.classList.toggle('description');
+        });
+
     }, 1500)
 
     
